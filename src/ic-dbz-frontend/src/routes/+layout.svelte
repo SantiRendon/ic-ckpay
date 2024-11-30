be5a5b41f5eeeb370de039078739afd038d078ea
SantiRendon <xantirendonc@gmail.com>
2024-11-30 13:57:35 -0500

    refactor home page

--- src/ic-dbz-frontend/src/routes/+layout.svelte
+++ src/ic-dbz-frontend/src/routes/+layout.svelte
@@ -2,13 +2,14 @@
   import "../index.css";
 </script>
 
-<slot />
-
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-<h1>Hello world!</h1>
+<div
+  class="bg-gray-100 dark:bg-gray-900 dark:text-white text-gray-600 h-screen flex overflow-hidden text-sm"
+>
+  <slot />
+</div>
 
 <style lang="postcss">
-  :global(html) {
+  /* :global(html) {
     background-color: theme(colors.gray.100);
-  }
+  } */
 </style>
