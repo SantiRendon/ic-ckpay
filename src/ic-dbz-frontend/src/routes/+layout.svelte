654aeb3847517339fd582b0c76c467190cf18257
SantiRendon <xantirendonc@gmail.com>
2024-11-30 15:37:07 -0500

    add components in layout.svelte

--- src/ic-dbz-frontend/src/routes/+layout.svelte
+++ src/ic-dbz-frontend/src/routes/+layout.svelte
@@ -1,11 +1,29 @@
 <script>
   import "../index.css";
+  import Navbar from "$lib/components/Navbar.svelte";
+  import Sidebar from "$lib/components/Sidebar.svelte";
+  import { UserList } from "$lib/components/user";
 </script>
 
 <div
   class="bg-gray-100 dark:bg-gray-900 dark:text-white text-gray-600 h-screen flex overflow-hidden text-sm"
 >
-  <slot />
+  <Sidebar />
+
+  <div class="flex-grow overflow-hidden h-full flex flex-col">
+    <Navbar />
+
+    <!-- Content -->
+    <div class="flex-grow flex overflow-x-hidden">
+      <!-- UserList -->
+      <UserList />
+
+      <!-- ActivityPanel -->
+      <div class="flex-grow bg-white dark:bg-gray-900 overflow-y-auto">
+        <slot />
+      </div>
+    </div>
+  </div>
 </div>
 
 <style lang="postcss">
--- src/ic-dbz-frontend/src/routes/+page.svelte
+++ src/ic-dbz-frontend/src/routes/+page.svelte
@@ -3,45 +3,19 @@
     ActivityFilters,
     ActivityTable,
     ActivityTableFooter,
-  } from "$lib/activity";
-
-  import { UserInfo, UserList } from "$lib/user";
-
-  import Navbar from "$lib/Navbar.svelte";
-
-  import Sidebar from "$lib/Sidebar.svelte";
-
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    console.log("mount");
-  });
+  } from "$lib/components/activity";
+  import { UserInfo } from "$lib/components/user";
 </script>
 
-<Sidebar />
+<!-- UserInfo -->
+<UserInfo />
 
-<div class="flex-grow overflow-hidden h-full flex flex-col">
-  <Navbar />
+<div class="sm:p-7 p-4">
+  <!-- ActivityFilters -->
+  <ActivityFilters />
 
-  <!-- Content -->
-  <div class="flex-grow flex overflow-x-hidden">
-    <!-- UserList -->
-    <UserList />
-
-    <!-- ActivityPanel -->
-    <div class="flex-grow bg-white dark:bg-gray-900 overflow-y-auto">
-      <!-- UserInfo -->
-      <UserInfo />
-
-      <div class="sm:p-7 p-4">
-        <!-- ActivityFilters -->
-        <ActivityFilters />
-
-        <!-- ActivityTable -->
-        <ActivityTable />
-        <!-- ActivityTableFooter -->
-        <ActivityTableFooter />
-      </div>
-    </div>
-  </div>
+  <!-- ActivityTable -->
+  <ActivityTable />
+  <!-- ActivityTableFooter -->
+  <ActivityTableFooter />
 </div>
