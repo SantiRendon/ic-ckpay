9f6f2d58fdf736191746a34f9e080d35f2d615da
SantiRendon <xantirendonc@gmail.com>
2024-11-30 12:37:52 -0500

    init ic-dbz

---
+++ src/ic-dbz-frontend/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../index.css";
+</script>
+
+<slot />
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<h1>Hello world!</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
