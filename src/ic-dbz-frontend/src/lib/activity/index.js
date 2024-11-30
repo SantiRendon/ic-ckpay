8fe90eb28a591be6ee74725c8470baaceac0850d
SantiRendon <xantirendonc@gmail.com>
2024-11-30 15:12:22 -0500

    organize components and create index.js for easier imports

---
+++ src/ic-dbz-frontend/src/lib/activity/index.js
@@ -0,0 +1,3 @@
+export { default as ActivityFilters } from "./ActivityFilters.svelte";
+export { default as ActivityTable } from "./ActivityTable.svelte";
+export { default as ActivityTableFooter } from "./ActivityTableFooter.svelte";
