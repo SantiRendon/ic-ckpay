c5a6b7b5f98a45ebba10ecde33904d047df1308b
SantiRendon <xantirendonc@gmail.com>
2024-12-29 16:29:40 -0500

    add app page

--- src/ic-dbz-frontend/src/routes/app/+page.svelte
+++ src/ic-dbz-frontend/src/routes/app/+page.svelte
@@ -1,4 +1,66 @@
 <script>
+  import { Add } from "carbon-icons-svelte";
+  import { DataTable } from "carbon-components-svelte";
+
+  import { Accordion, AccordionItem } from "carbon-components-svelte";
 </script>
 
-<h1>Hola</h1>
+<Add />
+
+<div>
+  <DataTable
+    title="Load balancers"
+    description="Your organization's active load balancers."
+    class="color: black"
+    headers={[
+      { key: "name", value: "Name" },
+      { key: "protocol", value: "Protocol" },
+      { key: "port", value: "Port" },
+      { key: "rule", value: "Rule" },
+    ]}
+    rows={[
+      {
+        id: "a",
+        name: "Load Balancer 3",
+        protocol: "HTTP",
+        port: 3000,
+        rule: "Round robin",
+      },
+      {
+        id: "b",
+        name: "Load Balancer 1",
+        protocol: "HTTP",
+        port: 443,
+        rule: "Round robin",
+      },
+      {
+        id: "c",
+        name: "Load Balancer 2",
+        protocol: "HTTP",
+        port: 80,
+        rule: "DNS delegation",
+      },
+      {
+        id: "d",
+        name: "Load Balancer 6",
+        protocol: "HTTP",
+        port: 3000,
+        rule: "Round robin",
+      },
+      {
+        id: "e",
+        name: "Load Balancer 4",
+        protocol: "HTTP",
+        port: 443,
+        rule: "Round robin",
+      },
+      {
+        id: "f",
+        name: "Load Balancer 5",
+        protocol: "HTTP",
+        port: 80,
+        rule: "DNS delegation",
+      },
+    ]}
+  />
+</div>
